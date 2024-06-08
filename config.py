ceee7980aa501faf1256890fc9be34c3b97b7ceb
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-06-08 17:25:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Rkn_Bots(object):
     # Rkn client config
     API_ID = os.environ.get("API_ID", "23171051")
     API_HASH = os.environ.get("API_HASH", "10331d5d712364f57ffdd23417f4513c")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7343805166:AAHtmz0TxF0mq5YmJGnEEFYSDc2GPH9vgyA")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7161447840:AAHijbJ3wANZcgdElBe2kkxYOsOaeS7wFUs")
 
     #start_pic
     RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
@@ -20,7 +20,7 @@ class Rkn_Bots(object):
     # wes response configuration
     BOT_UPTIME = time.time()
     PORT = int(os.environ.get("PORT", "8080"))
-    FORCE_SUB = os.environ.get("FORCE_SUB", "-1002217710548") 
+    FORCE_SUB = os.environ.get("FORCE_SUB", "-1001868502293") 
     
     # database config
     DB_NAME = os.environ.get("DB_NAME", "Lucifer")     
@@ -28,14 +28,14 @@ class Rkn_Bots(object):
 
     #caption
     DEF_CAP = os.environ.get("DEF_CAP",
-                             "<b><a href='telegram.me/Rkn_Bots'>{file_name} Telegram : @RknDeveloper\n\nForward the file before Downloading.</a></b>",
+                             "<b><a href='https://t.me/TMR_update_channel'>{file_name} Telegram : @TMR_DEVELOPER\n\nForward the file before Downloading.</a></b>",
     )
 
     #sticker Id
-    STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAELFqBllhB70i13m-woXeIWDXU6BD2j7wAC9gcAAkb7rAR7xdjVOS5ziTQE")
+    STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAKjYmZkRgOAtKRZ1d8LHnLAKUtus9noAAIjAAMoD2oUJ1El54wgpAY1BA")
 
     #admin id
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6987799874').split()]
     
 
 # Rkn Developer 
